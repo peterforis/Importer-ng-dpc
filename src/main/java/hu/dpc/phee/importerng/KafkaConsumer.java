f19ba99f1dadf9c8610189d9d9a454bbdcaeca8f
Peter Foris <peterforis6@gmail.com>
2021-08-06 16:06:43 +0200

    Implement filtering on field intent

--- src/main/java/hu/dpc/phee/importerng/KafkaConsumer.java
+++ src/main/java/hu/dpc/phee/importerng/KafkaConsumer.java
@@ -2,6 +2,7 @@ package hu.dpc.phee.importerng;
 
 import com.bazaarvoice.jolt.Chainr;
 import com.bazaarvoice.jolt.JsonUtils;
+import org.json.JSONObject;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -15,19 +16,30 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "zeebe-export", groupId = "importer-ng")
     public void listenToPartition(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-        System.out.println("Basic message logging: " + message + " from partition: " + partition);
-        System.out.println("Joltparsed message: " + joltParse(message) + " from partition: " + partition);
+        String joltparsedJson = joltParse(message, "/spec_filter_COMPLETED.json");
+        if (joltparsedJson == null) {
+            System.out.println("New basic message: " + message);
+        } else {
+            System.out.println("New joltparsed message: " + joltparsedJson);
+        }
     }
 
-    public String joltParse(String message) {
+    public String joltParse(String message, String parserPath) {
         try {
-            List<Object> chainrSpecJSON = JsonUtils.classpathToList("/spec.json");
+            List<Object> chainrSpecJSON = JsonUtils.classpathToList(parserPath);
             Chainr chainr = Chainr.fromSpec(chainrSpecJSON);
             Object transformedOutput = chainr.transform(JsonUtils.jsonToObject(message));
+            if (transformedOutput == null) {
+                return null;
+            }
             return JsonUtils.toJsonString(transformedOutput);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public String jsonPrettyPrint(String jsonString) {
+        return new JSONObject(jsonString).toString();
     }
 }
